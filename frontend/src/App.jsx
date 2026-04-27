bc93bd31b87ce13d1b3633222e1a7f32d86b1e52
tgarvs <tgarvey12@gmail.com>
2026-04-27 16:00:29 +0100

    visualing data in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react'
 import SpotSelector from './components/SpotSelector'
 
 function App() {
+  const[selectedSpot, setSelectedSpot] = useState(null)
+  const[forecasts, setForecast] = useState(null)
+
+    useEffect(() => {
+        if(selectedSpot == null) return
+        fetch('http://localhost:8000/forecast/' + selectedSpot)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setForecast(data)
+        })
+    }, [selectedSpot])
+
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold">Swell</h1>
-      <SpotSelector />
+      <SpotSelector onSpotSelect={setSelectedSpot}/>
+      <p className="text-1xl">{selectedSpot}</p>
+      {forecasts && forecasts.readings.map(reading => 
+        <div key={reading.time}>
+          <p className="text-1xl font-italics">Wind Time: {reading.time}</p>
+          <p className="text-1xl font-italics">Wind Speed: {reading.wind_speed}</p>
+          <p className="text-1xl font-italics">Wind Direction: {reading.wind_direction}</p>
+          <p className="text-1xl font-italics">Wave Direction: {reading.wave_direction}</p>
+          <p className="text-1xl font-italics">Wave Period: {reading.wave_period}</p>
+          <p className="text-1xl font-italics">Wave Height: {reading.wave_height}</p>
+        </div>
+      )}
+
     </div>
   )
 }
--- frontend/src/components/SpotSelector.jsx
+++ frontend/src/components/SpotSelector.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 
 
 
-function SpotSelector (){
-    const[spotList, setSpots] = useState([]) //need brackets bc we are going to pass in a list
+function SpotSelector ({onSpotSelect}){
+    const[spotList, setSpots] = useState([]) //need brackets bc we are going to pass in a list, this creates a state and state set 
 
+    // this runs once when the component loads bc of the ending []
     useEffect(() => {
         fetch('http://localhost:8000/spots')
         .then(res => res.json())
@@ -15,18 +16,17 @@ function SpotSelector (){
         })
     }, [])
 
-  return ( //what does <div> do?
+
+  return (
     <div> 
       {spotList.map(spot => ( //iterate over spotlist using map
-        <button 
-        key={spot.id}
-        className="m-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        >
-        {spot.name}
-        </button>      ))}
+        <button onClick={() => onSpotSelect(spot.id)}
+                key={spot.id}
+                className="m-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >{spot.name} </button>      ))}
     </div>
   )
 }
 
 
-export default SpotSelector //what does this do?
+export default SpotSelector 
